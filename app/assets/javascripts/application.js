26b0732d0eec82dac1ca2431740a6f9291700734
rodolfo <rodolfo.perezg0@gmail.com>
2019-09-10 21:27:34 -0300

    try again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require rails-ujs
 //= require popper
 //= require activestorage
+//= require jquery
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
